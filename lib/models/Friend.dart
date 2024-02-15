454cd7ae694da8357c4174b82c463e444bedf40e
Brandon Plyler <brandonplyler1768@gmail.com>
2024-02-15 15:16:54 -0500

    Added test and friend class

--- lib/models/Friend.dart
+++ lib/models/Friend.dart
@@ -7,7 +7,6 @@ class Friend {
   String? aboutMe;
   // list of gyms here
 
-
   // getters
   String getUserName() {
     return userName!;
@@ -23,5 +22,22 @@ class Friend {
 
   String getAboutMe() {
     return aboutMe!;
+  }
+
+  // setters
+  void setUserName(String name) {
+    userName = name;
+  }
+
+  void setPassword(String p) {
+    password = p;
+  }
+
+  void setEmail(String e) {
+    email = e;
+  }
+
+  void setAboutMe(String a) {
+    aboutMe = a;
   }
 }
