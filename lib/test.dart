baa7185bb1fa0192f50ac8eff09026a5d68255fc
Brandon Plyler <brandonplyler1768@gmail.com>
2024-02-16 10:33:22 -0500

    Testing classes in dart

--- lib/test.dart
+++ lib/test.dart
@@ -1,3 +1,11 @@
+import 'models/Friend.dart';
+// File Strictly for learning how to use dart
+
 void main() {
-  
+  Friend f = Friend();
+  f.setUserName('RandomName');
+  f.setAboutMe('A Person Looking For Gym Friends!');
+  f.setEmail('RandomEmail@gmail.com');
+  f.setPassword('RandomPassword');
+  print(f.getAboutMe() + f.getEmail() + f.getUserName() + f.getPassword());
 }
