911609e5fd64882bd42893fd51acd89d7cad18f6
Michael Gogarty <mcg1441@uncw.edu>
2024-02-19 14:22:44 -0500

    Added intitial outline for profile class

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -1,3 +1,21 @@
+import '../models/gym.dart';
+
 class Profile {
-  
+  String username;
+  String password;
+  String email;
+  String bio;
+  // Using NULL safety through the '?' operator.
+  // Ensure this is handled appropriately.
+  Gym? gym;
+
+  Profile({
+    // The following are required parameters for a profile to exist.
+    required this.username,
+    required this.password,
+    required this.email,
+    required this.bio,
+    // The user may not necessarily have a gym, so it is optional.
+    this.gym,
+    });
 }
