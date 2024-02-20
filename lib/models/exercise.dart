e9744f2443850181b3b2db79d45836accb43cb8f
rileysimpsom <res7348@uncw.edu>
2024-02-20 14:53:55 -0500

    "put sample classes outside of class code brackets . deleted trackerlog class and indiviudally profiled the developed classes."

--- lib/models/exercise.dart
+++ lib/models/exercise.dart
@@ -1,2 +1,16 @@
 class Exercise {
+  int sets;
+  int reps;
+  String name;
+  String group;
+
+  Exercise(
+      {required this.sets,
+      required this.reps,
+      required this.name,
+      required this.group});
 }
+List<Exercise> sampleExercises = [
+    Exercise(sets: 3, reps: 12, name: 'Push-ups', group: 'Chest'),
+    Exercise(sets: 4, reps: 10, name: 'Squats', group: 'Legs'),
+  ];
