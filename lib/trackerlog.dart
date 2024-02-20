e2c490ef680feb22ec5f7aaecd56ea3d26552e3e
rileysimpsom <res7348@uncw.edu>
2024-02-20 14:29:09 -0500

    "Formatted file correctly."

--- lib/trackerlog.dart
+++ lib/trackerlog.dart
@@ -4,10 +4,14 @@ class Exercise {
   String name;
   String group;
 
-  Exercise({required this.sets, required this.reps, required this.name, required this.group});
+  Exercise(
+      {required this.sets,
+      required this.reps,
+      required this.name,
+      required this.group});
 
   static List<Exercise> sampleExercises = [
-    Exercise(sets: 3, reps: 12, name: 'Push-ups', group:'Chest'),
+    Exercise(sets: 3, reps: 12, name: 'Push-ups', group: 'Chest'),
     Exercise(sets: 4, reps: 10, name: 'Squats', group: 'Legs'),
   ];
 }
@@ -17,10 +21,14 @@ class Workout {
   String name;
   int duration; // in minutes
 
-  Workout({required this.exercises, required this.name, required this.duration});
+  Workout(
+      {required this.exercises, required this.name, required this.duration});
 
   static List<Workout> sampleWorkouts = [
-    Workout(exercises: Exercise.sampleExercises, name: 'Morning Routine', duration: 30),
+    Workout(
+        exercises: Exercise.sampleExercises,
+        name: 'Morning Routine',
+        duration: 30),
   ];
 }
 
@@ -31,6 +39,7 @@ class WorkoutRecord {
   WorkoutRecord({required this.workout, required this.dateTime});
 
   static List<WorkoutRecord> sampleRecords = [
-    WorkoutRecord(workout: Workout.sampleWorkouts.first, dateTime: DateTime.now()),
+    WorkoutRecord(
+        workout: Workout.sampleWorkouts.first, dateTime: DateTime.now()),
   ];
 }
