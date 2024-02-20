4fb5c9728413e716a4f9fe1ede93b9eca8d9b73f
landon-lc <casstevens75@gmail.com>
2024-02-20 14:38:31 -0500

    added the user class, which extends the profile class.

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,15 @@
-class User {
+import '../models/friend.dart';
+import '../models/workout.dart';
+import '../models/workout_record.dart';
+import '../models/exercise.dart';
+import '../models/profile.dart';
+
+class User extends Profile {
   
+  // Dynamic sets for user data.  
+  var friendsList = <Friend>{};
+  var workoutTemplates = <Workout>{};
+  var workouts = <WorkoutRecord>{};
+  var exercises = <Exercise>{};
+
 }
