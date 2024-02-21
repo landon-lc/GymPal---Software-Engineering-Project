4cc232cee12c91272af991d38dd1d16c5a0a30e4
Brandon Plyler <brandonplyler1768@gmail.com>
2024-02-21 18:13:49 -0500

    Added more functionality to the Frineds class

--- lib/models/friend.dart
+++ lib/models/friend.dart
@@ -1,39 +1,39 @@
 import 'profile.dart'; // Use for the class variables
+import 'gym.dart';
 
 class Friend {
   // this will later select certain attributes from the profile class
-  String userName;
+  String? userName; // may need to make this non nullable
   String? password;
   String? email;
   // find a way to add a profile picture
   String? aboutMe;
   // list of gyms here
-  var gyms = []; // make this settable
+   List<String> gym = []; // this is temporarily going to be a String
 
   Friend({
     required this.userName,
     required this.password,
     required this.email,
     required this.aboutMe
-    this.gyms
-  })
+  });
 
   // getters
 
   String getUserName() {
-    return userName!;
+    return this.userName!;
   }
 
   String getPassword() {
-    return password!;
+    return this.password!;
   }
 
   String getEmail() {
-    return email!;
+    return this.email!;
   }
 
   String getAboutMe() {
-    return aboutMe!;
+    return this.aboutMe!;
   }
 
   // setters
@@ -57,11 +57,11 @@ class Friend {
   //random methods (These aren't working currently)
 
   void addGym(String g) {
-    gyms.add(g);
+    this.gym.add(g);
   }
 
   void displayGyms() {
-    for (String g in gyms) {
+    for (String g in this.gym) {
       print(g);
     }
   }
--- test/friend_test.dart
+++ test/friend_test.dart
@@ -2,11 +2,7 @@ import '../lib/models/friend.dart';
 // File Strictly for learning how to use dart
 
 void main() {
-  Friend f = Friend();
-  f.setUserName('RandomName');
-  f.setAboutMe('A Person Looking For Gym Friends!');
-  f.setEmail('RandomEmail@gmail.com');
-  f.setPassword('RandomPassword');
+  Friend f = Friend(userName:'RandomName', aboutMe: 'A Person Looking For Gym Friends!', email: 'RandomEmail@gmail.com', password: 'RandomPassword');
   f.addGym("Planet Fitness");
   f.addGym("Golds Gym");
   print(f.getAboutMe() + '\n' + f.getEmail() + '\n' + f.getUserName() +  '\n' + f.getPassword()); // use interpolation to compose strings?
