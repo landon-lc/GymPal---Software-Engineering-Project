1d06674431072e018acdab16439b6c321d8a50c5
Brandon Plyler <brandonplyler1768@gmail.com>
2024-02-22 00:12:28 -0500

    Added functionality to Friends class

--- test/friend_test.dart
+++ test/friend_test.dart
@@ -5,5 +5,4 @@ import '../lib/models/gym.dart';
 void main() {
   Friend f = Friend(userName:'RandomName', aboutMe: 'A Person Looking For Gym Friends!', email: 'RandomEmail@gmail.com', password: 'RandomPassword');
   print(f.getAboutMe() + '\n' + f.getEmail() + '\n' + f.getUserName() +  '\n' + f.getPassword()); // use interpolation to compose strings?
-  f.displayGyms();
 }
