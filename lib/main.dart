6043610fbca7cbb0b5459c91ceda4a6281ffe34a
landon-lc <casstevens75@gmail.com>
2024-02-22 17:47:10 -0500

    Changing main method to async - does not impact running, but is needed for firebase initializing via main.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
