17c2565194888bfc431b511203ac1b88f138a53c
Michael Gogarty <mcg1441@uncw.edu>
2024-02-22 12:02:01 -0500

    Attempt to fix dependency issues.

--- test/friend_test.dart
+++ test/friend_test.dart
@@ -1,4 +1,4 @@
-import '../lib/models/friend.dart';
+import 'package:test_drive/models/friend.dart';
 // File Strictly for learning how to use dart
 
 void main() {
@@ -7,8 +7,8 @@ void main() {
   f.setAboutMe('A Person Looking For Gym Friends!');
   f.setEmail('RandomEmail@gmail.com');
   f.setPassword('RandomPassword');
-  f.addGym("Planet Fitness");
-  f.addGym("Golds Gym");
-  print(f.getAboutMe() + '\n' + f.getEmail() + '\n' + f.getUserName() +  '\n' + f.getPassword()); // use interpolation to compose strings?
+  f.addGym('Planet Fitness');
+  f.addGym('Golds Gym');
+  print('${f.getAboutMe()}\n${f.getEmail()}\n${f.getUserName()}\n${f.getPassword()}'); // use interpolation to compose strings?
   f.displayGyms();
 }
