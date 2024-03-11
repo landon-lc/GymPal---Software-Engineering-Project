4331d336f266687d014bdbb8135fc7e12dd27c56
rileysimpsom <res7348@uncw.edu>
2024-03-11 14:54:44 -0400

    added checklist page file

---
+++ lib/pages/checklist_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChecklistPage extends StatelessWidget {
+  const ChecklistPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+      );
+  }
+}
