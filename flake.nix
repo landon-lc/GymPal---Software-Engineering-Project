34de36d3598b682e13daf6a18ba40b45f58f3e0a
Michael Gogarty <mcg1441@uncw.edu>
2024-03-12 16:19:18 -0400

    Added files requisite for my developer environment

---
+++ flake.nix
@@ -0,0 +1,30 @@
+# For Michael's dev environment :)
+{
+description = "Flutter 3.13.x";
+inputs = {
+  nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  flake-utils.url = "github:numtide/flake-utils";
+};
+outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      devShell =
+        with pkgs; mkShell rec {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          buildInputs = [
+            flutter
+            androidenv.androidPkgs_9_0.androidsdk
+            jdk17
+          ];
+        };
+    });
+}
