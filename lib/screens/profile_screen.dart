473e2d7f9836888c820fa51943871666510eceda
landon-lc <casstevens75@gmail.com>
2024-03-13 17:39:35 -0400

    Very basic profile screen

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UserName extends StatelessWidget {
+  const UserName({
+    super.key,
+    required this.profileUserName,
+  });
+
+  final String profileUserName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Text(
+        profileUserName,
+        softWrap: true,
+      ),
+    );
+  }
+}
