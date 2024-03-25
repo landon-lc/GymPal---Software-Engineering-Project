d7639906ca870900cab9dd69edea5519264df39a
Landon Casstevens <casstevens75@gmail.com>
2024-03-24 21:47:34 -0400

    Account creation screen - added fields needed for creating an account

--- lib/screens/account_creation_screen.dart
+++ lib/screens/account_creation_screen.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 
 // The user is creating an account for the first time. 
-class AccountCreationScreen extends StatelessWidget {
+class AccountCreationScreen extends StatefulWidget {
   const AccountCreationScreen({super.key});
 
   @override
+  State<AccountCreationScreen> createState() => _AccountCreationScreen();
+
+}
+
+class _AccountCreationScreen extends State<AccountCreationScreen> {
+final newUsernameController = TextEditingController();
+final newPasswordController = TextEditingController();
+final newEmailController = TextEditingController();
+
+@override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            body: SingleChildScrollView(
-                child: Column(children: [
-                   Text('placeholder')
-        ]))));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Account Creation'),
+        ),
+        body: Center(
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextField(
+                        decoration: const InputDecoration(labelText: 'Create a Username'),
+                        controller: newUsernameController,
+                      ),
+                      TextField(
+                        decoration:
+                            const InputDecoration(labelText: 'Create a Password'),
+                        // MAY SET TO TRUE LATER - DO NOT DELETE
+                        obscureText: false,
+                        controller: newPasswordController,
+                      ),
+                      TextField(
+                        decoration: const InputDecoration(labelText: 'Enter your Email'),
+                        controller: newEmailController,
+                      ),
+                    ]))));
   }
 }
