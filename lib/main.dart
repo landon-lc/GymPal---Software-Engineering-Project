4b85b108236f98f4cd8315fb89fe24e04436e681
Brandon Plyler <brandonplyler1768@gmail.com>
2024-03-24 21:40:31 -0400

    Connected Friends Screen to nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
     const PlaceholderWidget(color: Colors.green),
     // Map Button
     const PlaceholderWidget(color: Colors.yellow),
-    const FriendsScreen(),
+    const FriendsScreen()
   ];
 
   @override
