b49870ee1ab7ae1cea6fde6903449d6af879d34d
jhburriss94 <jhburriss94@gmail.com>
2024-03-25 21:16:46 -0400

    Making changes, trying to build the map with markers

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -6,43 +5,21 @@ class GymMaps extends StatefulWidget {
   const GymMaps({super.key});
 
   @override
-  State<GymMaps> createState() => GymMapsState();
+  State<GymMaps> createState() => _GymMapsState();
 }
 
-class GymMapsState extends State<GymMaps> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(34.20901806593137, -77.89428829935258),
-    zoom: 12,
-  );
-
-  static const CameraPosition _kO2FitnessRacine = CameraPosition(
-      target: LatLng(34.247339250011464, -77.86304592936503),
-      tilt: 59.440717697143555,
-      zoom: 18);
+class _GymMapsState extends State<GymMaps> {
+  static const LatLng _pGooglePlex = LatLng(34.20901806593137, -77.89428829935258);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('Take me to O2 Fitness'),
-        icon: const Icon(Icons.directions_boat),
+        initialCameraPosition: CameraPosition(
+          target: _pGooglePlex,
+          zoom: 11,
+        ),
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kO2FitnessRacine));
   }
 }
