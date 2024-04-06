1cf8120998d154149183a2fbb039dccbcf69e7fb
Landon Casstevens <casstevens75@gmail.com>
2024-04-05 23:43:09 -0400

    Image upload not working. Will tackle another day

--- lib/screens/profile_editor_screen.dart
+++ lib/screens/profile_editor_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'profile_screen.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class ProfileEditorScreen extends StatefulWidget {
   const ProfileEditorScreen ({super.key});
@@ -17,7 +19,7 @@ class _ProfileEditorScreen extends State<ProfileEditorScreen> {
           body: SingleChildScrollView(
             child: Column(children: [
               // Implement sub-screens.
-              ProfileEditorScreenFeatures(),
+              ImageUploader(),
               ProfileEditorBackButton(),
             ])
           )
@@ -26,50 +28,65 @@ class _ProfileEditorScreen extends State<ProfileEditorScreen> {
   }
 }
 
+// Getting the users profile picture. Not database integreated yet. 
+// Using code from https://flutterone.com/how-to-implement-an-image-picker-and-uploader-in-flutter/
+class ImageUploader extends StatefulWidget {
+  const ImageUploader({super.key});
 
-// // Handler for the profile editing screen.
-// class ProfileEditorScreen extends StatelessWidget {
-//   const ProfileEditorScreen({
-//     super.key,
-//   });
+  @override 
+  State<ImageUploader> createState() => _ImageUploader();
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Padding(
-//         padding: EdgeInsets.only(top: 40),
-//         child: Column(children: [
-//           ProfileEditorScreenFeatures(),
-//           ProfileEditorBackButton()
-//         ]));
-//   }
-// }
+class _ImageUploader extends State<ImageUploader> {
+  File? userImage;
 
-// Placeholder for future profile editor screen features.
-class ProfileEditorScreenFeatures extends StatelessWidget {
-  const ProfileEditorScreenFeatures({
-    super.key,
-  });
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: source);
 
-  @override
+    setState(() {
+      if (pickedImage != null) {
+        userImage = File(pickedImage.path);
+      }
+    });
+  }
+
+  @override 
   Widget build(BuildContext context) {
-    return const Align(
-        alignment: Alignment.center,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "This page ain't done yet.",
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile Editor'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          child: Column( 
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              if (userImage != null)
+                Image.file(
+                  userImage!,
+                ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _pickImage(ImageSource.gallery), 
+                child: const Text('Upload Image from Gallery'),
               ),
-            ),
-          ],
-        ));
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _pickImage(ImageSource.camera), 
+                child: const Text('Use Camera')
+              ),
+            ]
+          )
+          )
+      )
+    );
   }
 }
 
-// Code for the profile editor back button.
+// Code for the profile editor back button. WILL BECOME SAVE BUTTON
 class ProfileEditorBackButton extends StatelessWidget {
   const ProfileEditorBackButton({
     super.key,
