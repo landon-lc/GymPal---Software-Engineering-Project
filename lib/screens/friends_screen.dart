ae6ba1bbc14aab24814e98f98f46639a46c1fe3c
Michael Gogarty <mcg1441@uncw.edu>
2024-04-08 13:52:50 -0400

    Formatting

--- lib/screens/friends_screen.dart
+++ lib/screens/friends_screen.dart
@@ -4,22 +4,26 @@ import 'package:flutter/widgets.dart';
 // import 'package:firebase_database/firebase_database.dart'; will be used for calling friends info from database
 // import 'package:firebase_auth/firebase_auth.dart';
 // import '../models/profile.dart'; Both of these import statements will be used to call the users friends list
-// import '../models/friend.dart'; 
+// import '../models/friend.dart';
 
 class FriendsScreen extends StatelessWidget {
   const FriendsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) { // This does not look like a lot, but this took me a long time to figure out (For Dr. Layman)
+  Widget build(BuildContext context) {
+    // This does not look like a lot, but this took me a long time to figure out (For Dr. Layman)
     return Scaffold(
-      body: ListView.builder(itemCount: 3, itemBuilder: (context, index) { // item count will change to the length of the list that is storing the friends of the user
-        return MySquare();
-      }),
+      body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            // item count will change to the length of the list that is storing the friends of the user
+            return MySquare();
+          }),
     );
   }
 }
 
-// Used for displaying friends usernames, don't really know how 
+// Used for displaying friends usernames, don't really know how
 class FriendsUsernames extends StatelessWidget {
   const FriendsUsernames({
     super.key,
@@ -32,7 +36,7 @@ class FriendsUsernames extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
-      child: Text (
+      child: Text(
         friendUsername, // you'll change this to something with flutter once we store friends
       ),
     );
@@ -51,7 +55,7 @@ class FriendsGyms extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
-      child: Text (
+      child: Text(
         friendGyms, // you'll change this to something with flutter once we store friends
       ),
     );
@@ -78,39 +82,42 @@ class FriendImages extends StatelessWidget {
 }
 
 // Getting an exception saying the render box was not layed out?
-class FriendsList extends StatelessWidget { // Used to display a list of people that are friends with the user
+class FriendsList extends StatelessWidget {
+  // Used to display a list of people that are friends with the user
 
-  const FriendsList({ // was a const before
+  const FriendsList({
+    // was a const before
     super.key,
     required this.friendsList,
   });
 
-  final List<String> friendsList; // This is temporary, change the type of the list to Friend
+  final List<String>
+      friendsList; // This is temporary, change the type of the list to Friend
 
   @override
-  Widget build(BuildContext context) { // used to display the actual list
-    return Scaffold( 
-      body: Container (
-      child: ListView.builder(
-        itemCount: friendsList.length, // Change this to a call to the length of the list of the users friends list friendsList.length
-        itemBuilder: (context, index) {
-          return Card();
-          }
-        )
-      )
-    );
+  Widget build(BuildContext context) {
+    // used to display the actual list
+    return Scaffold(
+        body: Container(
+            child: ListView.builder(
+                itemCount: friendsList
+                    .length, // Change this to a call to the length of the list of the users friends list friendsList.length
+                itemBuilder: (context, index) {
+                  return Card();
+                })));
   }
 }
 
-class MySquare extends StatelessWidget { // This is used to define the square for each section of the list
+class MySquare extends StatelessWidget {
+  // This is used to define the square for each section of the list
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-      child: Container(
-        height: 20,
-        color: Colors.deepPurple, // color can change, just a random color I chose 
-      )
-    );
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+        child: Container(
+          height: 20,
+          color: Colors
+              .deepPurple, // color can change, just a random color I chose
+        ));
   }
 }
