70a86d73d889cb3a05c1558aa1c5028b4a3a997d
rileysimpsom <res7348@uncw.edu>
2024-04-12 15:24:32 -0400

    added userId references for databse implementations

--- lib/models/workout_record.dart
+++ lib/models/workout_record.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:test_drive/models/exercise.dart';
 import 'package:test_drive/models/workout.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class WorkoutRecord extends ChangeNotifier {
   List<Workout> workoutList = [];
   final DatabaseReference dbRef = FirebaseDatabase.instance.ref();
+  final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
 
   WorkoutRecord() {
     initWorkouts();
-  }
+ }
 
   Stream<List<Workout>> get workoutsStream =>
-      dbRef.child('workouts').onValue.map((event) {
+      dbRef.child('users/$userId/workouts').onValue.map((event) {
         final data = event.snapshot.value;
         if (data is Map<dynamic, dynamic>) {
           return data.entries
@@ -32,22 +34,21 @@ class WorkoutRecord extends ChangeNotifier {
   }
 
   void addWorkout(String name) {
-    final newWorkoutRef = dbRef.child('workouts').push();
-    final newWorkout =
-        Workout(name: name, exercises: [], key: newWorkoutRef.key);
-
-    newWorkoutRef.set(newWorkout.toMap()).then((_) {
+    final newWorkoutRef = dbRef.child('users/$userId/workouts').push();
+    newWorkoutRef.set({
+      'name': name,
+      'exercises': [],
+    }).then((_) {
+      workoutList.add(Workout(name: name, exercises: [], key: newWorkoutRef.key));
+      notifyListeners();
       print('Workout added successfully with key ${newWorkoutRef.key}');
     }).catchError((error) {
       print('Failed to add workout: $error');
     });
-    workoutList.add(newWorkout);
-    notifyListeners();
   }
 
-  void addExercises(String workoutId, String exerciseName, String weight,
-      String reps, String sets) {
-    dbRef.child('workouts/$workoutId/exercises').push().set({
+  void addExercises(String workoutId, String exerciseName, String weight, String reps, String sets) {
+    dbRef.child('users/$userId/workouts/$workoutId/exercises').push().set({
       'name': exerciseName,
       'weight': weight,
       'reps': reps,
@@ -57,38 +58,37 @@ class WorkoutRecord extends ChangeNotifier {
   }
 
   void updateWorkoutName(String workoutId, String newName) {
-    dbRef.child('workouts/$workoutId').update({
+    dbRef.child('users/$userId/workouts/$workoutId').update({
       'name': newName,
     }).then((_) {
-      print('Workout name updated successfully');
+      int index = workoutList.indexWhere((workout) => workout.key == workoutId);
+      if (index != -1) {
+        workoutList[index].name = newName;
+        notifyListeners();
+      }
     }).catchError((error) {
       print('Failed to update workout name: $error');
     });
-    int index = workoutList.indexWhere((workout) => workout.key == workoutId);
-    if (index != -1) {
-      workoutList[index].name = newName;
-      notifyListeners();
-    }
   }
 
   void checkOffExercise(String workoutId, String exerciseId, bool isCompleted) {
-    dbRef.child('workouts/$workoutId/exercises/$exerciseId').update({
+    dbRef.child('user/$userId/workouts/$workoutId/exercises/$exerciseId').update({
       'isCompleted': isCompleted,
     });
   }
 
   void deleteWorkout(String workoutId) {
-    dbRef.child('workouts/$workoutId').remove().then((_) {
+    dbRef.child('users/$userId/workouts/$workoutId').remove().then((_) {
+      workoutList.removeWhere((workout) => workout.key == workoutId);
+      notifyListeners();
       print('Workout deleted successfully from Firebase');
     }).catchError((error) {
       print('Failed to delete workout: $error');
     });
-    workoutList.removeWhere((workout) => workout.key == workoutId);
-    notifyListeners();
   }
 
   Stream<List<Exercise>> getExercisesStream(String workoutId) {
-    return dbRef.child('workouts/$workoutId/exercises').onValue.map((event) {
+    return dbRef.child('users/$userId/workouts/$workoutId/exercises').onValue.map((event) {
       final data = event.snapshot.value;
       if (data is Map<dynamic, dynamic>) {
         return data.entries
