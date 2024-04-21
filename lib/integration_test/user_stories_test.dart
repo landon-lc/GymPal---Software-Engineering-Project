15390025241bb179e5680e8325fa55ee71efa5ab
rileysimpsom <res7348@uncw.edu>
2024-04-21 11:08:40 -0400

    reformatting of folder

--- lib/integration_test/user_stories_test.dart
+++ lib/integration_test/user_stories_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import '../lib/main.dart';
+import 'package:test_drive/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
