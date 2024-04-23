b5907230f248104794f51860b83f9b800a754784
Brandon Plyler <brandonplyler1768@gmail.com>
2024-04-23 00:15:30 -0400

    Formatting

--- lib/src/screens/friends_profile_screen.dart
+++ lib/src/screens/friends_profile_screen.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 class FriendsProfileScreen extends StatelessWidget {
   final Map<dynamic, dynamic> user;
 
-
   const FriendsProfileScreen({super.key, required this.user});
-
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +11,11 @@ class FriendsProfileScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(user['username']),
       ),
-        body: Column(children: [
-          Expanded (
-            child: Text('Bio: ' + user['bio'])
-          ),
-          Expanded (
-            child: Text('Email: ' + user['email'])
-          ),
-          Expanded (
-            child: Text('Favorite Gym: ' + user['favGym'])
-          ),
-        ]
-      ),
+      body: Column(children: [
+        Expanded(child: Text('Bio: ' + user['bio'])),
+        Expanded(child: Text('Email: ' + user['email'])),
+        Expanded(child: Text('Favorite Gym: ' + user['favGym'])),
+      ]),
     );
   }
 }
