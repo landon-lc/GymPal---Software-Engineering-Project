30dee02b3055eb32a5ee52cbc75e22c3fdf5edef
Landon Casstevens <casstevens75@gmail.com>
2024-04-22 23:50:45 -0400

    random

--- lib/src/screens/friends_profile_screen.dart
+++ lib/src/screens/friends_profile_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import './profile_editor_screen.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 class FriendsProfileScreen extends StatelessWidget {
   final Map<dynamic, dynamic> user;
 
+
   const FriendsProfileScreen({super.key, required this.user});
+
 
   @override
   Widget build(BuildContext context) {
