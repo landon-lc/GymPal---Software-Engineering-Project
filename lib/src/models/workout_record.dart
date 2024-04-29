cdfaac8e7f888043cc400c7c292e280b411d29b1
rileysimpsom <res7348@uncw.edu>
2024-04-29 14:25:38 -0400

    bug fixes

--- lib/src/models/workout_record.dart
+++ lib/src/models/workout_record.dart
@@ -4,59 +4,29 @@ import 'package:test_drive/src/models/exercise.dart';
 import 'package:test_drive/src/models/workout.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-/// Manages the state and database operations for workout records in a Firebase database.
-///
-/// This class provides functionality to create, read, update, and delete workouts
-/// and exercises for a specific user. It uses Firebase Realtime Database to store and manage data.
-
 class WorkoutRecord extends ChangeNotifier {
   List<Workout> workoutList = [];
-
-  /// Firebase database reference used to perform CRUD operations.
   final DatabaseReference dbRef = FirebaseDatabase.instance.ref();
-
-  /// The current user's ID from FirebaseAuth, used to scope data access.
   final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
 
-  /// Tracks the current page for pagination of workouts.
-  int _currentPage = 0;
-
-  /// Defines the number of workouts to be loaded per page.
-  final int _pageSize = 10; // Number of workouts per page
-
-  /// Initializes workouts by setting up a listener on the paginated workout stream.
   WorkoutRecord() {
     initWorkouts();
-  }
+ }
 
-  /// Provides a stream of paginated workouts, optionally filtered by a start and end date.
-  ///
-  /// Each call retrieves one page of workouts. Subsequent calls retrieve the next page.
-  /// If `start` and/or `end` are provided, filters the workouts to those within the given date range.
-  Stream<List<Workout>> paginatedWorkoutsStream(
-      {DateTime? start, DateTime? end}) {
-    Query query = dbRef
-        .child('users/$userId/workouts')
-        .orderByChild('timestamp')
-        .limitToFirst((_currentPage + 1) * _pageSize);
-
-    if (start != null && end == null) {
-      // Assuming end date is today if not provided
-      end = DateTime.now();
+  Stream<List<Workout>> workoutsStream({DateTime? start, DateTime? end}) {
+    Query query = dbRef.child('users/$userId/workouts').orderByChild('timestamp');
+    if (start != null) {
+      query = query.startAt(start.toIso8601String());
     }
-    if (start == null && end != null) {
-      // Assuming start date is a reasonable past limit if not provided
-      start = DateTime.now().subtract(const Duration(days: 365));
+    if (end != null) {
+      query = query.endAt(end.toIso8601String());
     }
-    if (start != null) query = query.startAt(start.toUtc().toIso8601String());
-    if (end != null) query = query.endAt(end.toUtc().toIso8601String());
 
     return query.onValue.map((event) {
       final data = event.snapshot.value;
       if (data is Map<dynamic, dynamic>) {
         return data.entries
-            .map((e) => Workout.fromMap(Map<String, dynamic>.from(e.value),
-                key: e.key.toString()))
+            .map((e) => Workout.fromMap(Map<String, dynamic>.from(e.value), key: e.key.toString()))
             .toList();
       } else {
         return [];
@@ -64,31 +34,22 @@ class WorkoutRecord extends ChangeNotifier {
     });
   }
 
-  /// Advances to the next page of workouts.
-  void nextPage() {
-    _currentPage++;
-    notifyListeners();
-  }
-
-  /// Initializes the workout list and sets up a listener to update local data on changes.
   Future<void> initWorkouts() async {
-    paginatedWorkoutsStream().listen((workouts) {
+    workoutsStream().listen((workouts) {
       workoutList = workouts;
       notifyListeners();
     });
   }
 
-  /// Adds a new workout to Firebase under the current user's ID with the current timestamp.
   void addWorkout(String name) {
     final newWorkoutRef = dbRef.child('users/$userId/workouts').push();
-    final now = DateTime.now().toUtc(); // Convert to UTC
+    final now = DateTime.now();
     newWorkoutRef.set({
       'name': name,
       'exercises': [],
       'timestamp': now.toIso8601String(),
     }).then((_) {
-      workoutList.add(Workout(
-          name: name, exercises: [], key: newWorkoutRef.key, timestamp: now));
+      workoutList.add(Workout(name: name, exercises: [], key: newWorkoutRef.key, timestamp: now));
       notifyListeners();
       print('Workout added successfully with key ${newWorkoutRef.key}');
     }).catchError((error) {
@@ -96,9 +57,7 @@ class WorkoutRecord extends ChangeNotifier {
     });
   }
 
-  /// Adds exercises to a specific workout in Firebase.
-  void addExercises(String workoutId, String exerciseName, String weight,
-      String reps, String sets) {
+  void addExercises(String workoutId, String exerciseName, String weight, String reps, String sets) {
     dbRef.child('users/$userId/workouts/$workoutId/exercises').push().set({
       'name': exerciseName,
       'weight': weight,
@@ -108,7 +67,6 @@ class WorkoutRecord extends ChangeNotifier {
     });
   }
 
-  /// Updates the name of an existing workout.
   void updateWorkoutName(String workoutId, String newName) {
     dbRef.child('users/$userId/workouts/$workoutId').update({
       'name': newName,
@@ -124,18 +82,15 @@ class WorkoutRecord extends ChangeNotifier {
   }
 
   void checkOffExercise(String workoutId, String exerciseId, bool isCompleted) {
-    dbRef
-        .child('users/$userId/workouts/$workoutId/exercises/$exerciseId')
-        .update({
+    dbRef.child('users/$userId/workouts/$workoutId/exercises/$exerciseId').update({
       'isCompleted': isCompleted,
-    }).then((_) {
-      print('Exercise status updated successfully.');
-    }).catchError((error) {
-      print('Failed to update exercise status: $error');
+      }).then((_) {
+    print('Exercise status updated successfully.');
+  }).catchError((error) {
+    print('Failed to update exercise status: $error');
     });
   }
 
-  /// Marks an exercise as completed or not completed.
   void deleteWorkout(String workoutId) {
     dbRef.child('users/$userId/workouts/$workoutId').remove().then((_) {
       workoutList.removeWhere((workout) => workout.key == workoutId);
@@ -146,7 +101,6 @@ class WorkoutRecord extends ChangeNotifier {
     });
   }
 
-  /// Edits the name of a workout.
   void editWorkout(String workoutId, String newName) {
     dbRef.child('users/$userId/workouts/$workoutId').update({'name': newName});
     int index = workoutList.indexWhere((workout) => workout.key == workoutId);
@@ -156,46 +110,33 @@ class WorkoutRecord extends ChangeNotifier {
     }
   }
 
-  /// Deletes an exercise from a workout.
   void deleteExercise(String workoutId, String exerciseId) {
-    dbRef
-        .child('users/$userId/workouts/$workoutId/exercises/$exerciseId')
-        .remove()
-        .then((_) {
-      print('Exercise deleted successfully from Firebase');
-      notifyListeners();
-    }).catchError((error) {
-      print('Failed to delete exercise: $error');
-    });
-  }
+  dbRef.child('users/$userId/workouts/$workoutId/exercises/$exerciseId').remove().then((_) {
+    print('Exercise deleted successfully from Firebase');
+    notifyListeners();  
+  }).catchError((error) {
+    print('Failed to delete exercise: $error');
+  });
+}
 
-  /// Edits an exercise in a workout.
-  void editExercise(String workoutId, String exerciseId, String newName,
-      String newWeight, String newReps, String newSets, bool newIsCompleted) {
-    dbRef
-        .child('users/$userId/workouts/$workoutId/exercises/$exerciseId')
-        .update({
-      'name': newName,
-      'weight': newWeight,
-      'reps': newReps,
-      'sets': newSets,
-      'isCompleted': newIsCompleted
-    }).then((_) {
-      print('Exercise updated successfully');
-      notifyListeners();
-    }).catchError((error) {
-      print('Failed to update exercise: $error');
-    });
-  }
+  void editExercise(String workoutId, String exerciseId, String newName, String newWeight, String newReps, String newSets, bool newIsCompleted) {
+  dbRef.child('users/$userId/workouts/$workoutId/exercises/$exerciseId').update({
+    'name': newName,
+    'weight': newWeight,
+    'reps': newReps,
+    'sets': newSets,
+    'isCompleted': newIsCompleted
+  }).then((_) {
+    print('Exercise updated successfully');
+    notifyListeners();  
+  }).catchError((error) {
+    print('Failed to update exercise: $error');
+  });
+}
 
-  /// Retrieves a stream of exercises for a specific workout.
-  ///
-  /// This method listens for real-time updates to a workout's exercises, reflecting any additions, deletions, or modifications.
+
   Stream<List<Exercise>> getExercisesStream(String workoutId) {
-    return dbRef
-        .child('users/$userId/workouts/$workoutId/exercises')
-        .onValue
-        .map((event) {
+    return dbRef.child('users/$userId/workouts/$workoutId/exercises').onValue.map((event) {
       final data = event.snapshot.value;
       if (data is Map<dynamic, dynamic>) {
         return data.entries
--- lib/src/screens/checklist_screen.dart
+++ lib/src/screens/checklist_screen.dart
@@ -1,40 +1,70 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:test_drive/src/models/workout_record.dart';
 import 'package:test_drive/src/models/workout.dart';
-import 'workout_screen.dart';
+import 'package:test_drive/src/screens/workout_screen.dart'; 
+import 'package:intl/intl.dart';
 
-/// A page that displays a list of workouts with options to edit, delete, or add new workouts.
-///
-/// This stateful widget uses [WorkoutRecord] to manage state and interact with the data model.
+
 class ChecklistPage extends StatefulWidget {
-  /// Constructs the [ChecklistPage] widget.
   const ChecklistPage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _ChecklistPageState createState() => _ChecklistPageState();
 }
 
-/// The state for [ChecklistPage] that handles user interaction and updates the UI accordingly.
-
 class _ChecklistPageState extends State<ChecklistPage> {
-  /// Controls the text field input for adding a new workout.
   final TextEditingController _controller = TextEditingController();
+  DateTime? startDate;
+  DateTime? endDate;
+
+  void _showDatePicker() async {
+    final DateTimeRange? picked = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime.now().subtract(const Duration(days: 365)),
+      lastDate: DateTime.now(),
+      initialDateRange: startDate != null && endDate != null
+          ? DateTimeRange(start: startDate!, end: endDate!)
+          : null,
+    );
+
+    if (picked != null) {
+      setState(() {
+        startDate = picked.start;
+        endDate = picked.end;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Workout Tracker')),
+      appBar: AppBar(
+        title: const Text('Workout Tracker'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.calendar_today),
+            onPressed: _showDatePicker,
+          ),
+          IconButton(
+            icon: const Icon(Icons.clear_all),
+            onPressed: () {
+              setState(() {
+                startDate = null;
+                endDate = null;
+              });
+            },    
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _createNewWorkout,
         child: const Icon(Icons.add),
       ),
       body: StreamBuilder<List<Workout>>(
-        stream: Provider.of<WorkoutRecord>(context, listen: false)
-            .paginatedWorkoutsStream(),
+        stream: Provider.of<WorkoutRecord>(context).workoutsStream(start: startDate, end: endDate),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -46,6 +76,14 @@ class _ChecklistPageState extends State<ChecklistPage> {
             return const Center(child: Text('No workouts found'));
           }
           final workouts = snapshot.data!;
+
+          Text(
+            startDate != null && endDate != null
+                ? 'Showing workouts from ${DateFormat('MM/dd/yyyy').format(startDate!)} to ${DateFormat('MM/dd/yyyy').format(endDate!)}'
+                : 'Showing All Workouts',
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
+          );
+
           return ListView.builder(
             itemCount: workouts.length,
             itemBuilder: (context, index) {
@@ -64,9 +102,7 @@ class _ChecklistPageState extends State<ChecklistPage> {
                     SlidableAction(
                       onPressed: (context) {
                         if (workout.key != null) {
-                          // Ensure workout.key is not null
-                          Provider.of<WorkoutRecord>(context, listen: false)
-                              .deleteWorkout(workout.key!);
+                          Provider.of<WorkoutRecord>(context, listen: false).deleteWorkout(workout.key!);
                         }
                       },
                       backgroundColor: Colors.red,
@@ -94,7 +130,6 @@ class _ChecklistPageState extends State<ChecklistPage> {
     );
   }
 
-  /// Opens a dialog to create a new workout.
   void _createNewWorkout() {
     showDialog(
       context: context,
@@ -110,8 +145,7 @@ class _ChecklistPageState extends State<ChecklistPage> {
             onPressed: () {
               final name = _controller.text.trim();
               if (name.isNotEmpty) {
-                Provider.of<WorkoutRecord>(context, listen: false)
-                    .addWorkout(name);
+                Provider.of<WorkoutRecord>(context, listen: false).addWorkout(name);
                 _controller.clear();
                 Navigator.pop(context);
               }
@@ -123,7 +157,6 @@ class _ChecklistPageState extends State<ChecklistPage> {
     );
   }
 
-  /// Opens a dialog to edit the name of an existing workout.
   void _editWorkout(String? workoutKey) {
     if (workoutKey == null) return;
     final TextEditingController editController = TextEditingController();
@@ -144,8 +177,7 @@ class _ChecklistPageState extends State<ChecklistPage> {
             onPressed: () {
               final newName = editController.text.trim();
               if (newName.isNotEmpty) {
-                Provider.of<WorkoutRecord>(context, listen: false)
-                    .editWorkout(workoutKey, newName);
+                Provider.of<WorkoutRecord>(context, listen: false).editWorkout(workoutKey, newName);
                 Navigator.pop(context);
               }
             },
